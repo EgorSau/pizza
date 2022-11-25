195f10ae92158d3864310bb52105e2fa3960a2f3
tfsaidov <tsaidov@uma.tech>
2022-11-25 20:57:57 +0300

    Refactoting Models 2

---
+++ FoodStore/FoodStore/CoreLayer/Models/Mock.swift
@@ -0,0 +1,54 @@
+//
+//  Presenter.swift
+//  FoodStore
+//
+//  Created by Egor SAUSHKIN on 13.10.2022.
+//
+
+import UIKit
+
+enum Section {
+    case banner
+    case category
+}
+
+struct Mock {
+    
+    static let shared = Mock()
+    
+    private init() {}
+    
+    func createMockData() -> [Section: Any] {
+        [
+            .banner: [
+                [
+                    PromoBanner(image: Data()),
+                    PromoBanner(image: Data()),
+                    PromoBanner(image: Data())
+                ]
+            ],
+            .category: [
+                [
+                    Category(name: "Пицца", priority: 1, isSelected: true): [
+                        Pizza(image: "1", title: "1", description: nil, price: 1),
+                        Pizza(image: "2", title: "2", description: nil, price: 2),
+                        Pizza(image: "3", title: "3", description: nil, price: 3),
+                        Pizza(image: "4", title: "4", description: nil, price: 4),
+                    ],
+                    Category(name: "Напитки", priority: 2, isSelected: false): [
+                        Pizza(image: "1", title: "1", description: nil, price: 1),
+                        Pizza(image: "2", title: "2", description: nil, price: 2),
+                        Pizza(image: "3", title: "3", description: nil, price: 3),
+                        Pizza(image: "4", title: "4", description: nil, price: 4),
+                    ],
+                    Category(name: "Соусы", priority: 3, isSelected: false): [
+                        Pizza(image: "1", title: "1", description: nil, price: 1),
+                        Pizza(image: "2", title: "2", description: nil, price: 2),
+                        Pizza(image: "3", title: "3", description: nil, price: 3),
+                        Pizza(image: "4", title: "4", description: nil, price: 4),
+                    ],
+                ]
+            ]
+        ]
+    }
+}
---
+++ FoodStore/FoodStore/CoreLayer/Models/PromoBanner.swift
@@ -0,0 +1,12 @@
+//
+//  PromoBanner.swift
+//  FoodStore
+//
+//  Created by Саидов Тимур on 25.11.2022.
+//
+
+import Foundation
+
+struct PromoBanner {
+    let image: Data
+}
